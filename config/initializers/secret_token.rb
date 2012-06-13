c5c5bdb298774e7886951d55d9d11ec764d70ba5
Rick Wash <wash@msu.edu>
2012-06-13 15:57:36 -0400

    Initial commit; blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdfundingExperiment::Application.config.secret_token = 'fb82205b69e26b1dc033068fd19510962b7548836f914e970cfc6363bc3993e0dafde1036a677685b8d9d58cdeb75710b8b544e33576f48b2847077de667b69e'
