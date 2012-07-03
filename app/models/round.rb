ccf577ef15786caf6d69a5d87b549f777dec523a
Jake Wesorick <wesoric6@msu.edu>
2012-07-03 09:41:24 -0400

    Fixed questions validation for quesiton 1.

--- app/models/round.rb
+++ app/models/round.rb
@@ -80,6 +80,8 @@ class Round < ActiveRecord::Base
 				# p.round_payout += Contribution.where(:user_id => p.user_id, :project_id => self.projects[3].id).first.amount
 			end
 			
+			self.finished = true
+			self.save!
 			p.save!
 		end
 	end
